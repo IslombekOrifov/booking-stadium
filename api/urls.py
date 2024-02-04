e5e86a4537d6239b5ebda4be601ca74ed9035734
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-05 02:38:23 +0500

    booking urls added

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('main/', include('main.urls', namespace='main')),
+    path('booking/', include('booking.urls', namespace='booking')),
 ]
