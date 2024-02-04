283f8f3bcdb8ebd5e3c3c8a08ce8f24de0718ca8
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-05 02:20:14 +0500

    v1: stadium list and detail serializers added

---
+++ api/main/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from apps.main.models import Stadium, StadiumImage
+
+
+class StadiumImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StadiumImage
+        fields = ['image']
+        
+
+class StadiumSerializer(serializers.ModelSerializer):
+    images = StadiumImageSerializer(many=True)
+    class Meta:
+        model = Stadium
+        fields = ['name', 'logo', 'address', 'description', 
+                  'contact1', 'contact2', 'price', 
+                  'start_working_time', 'end_working_time', 
+                  'lat', 'long', 'images']
+        
+
+class StadiumListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stadium
+        fields = ['name', 'logo', 'address', 'start_working_time', 
+                  'end_working_time', 'lat', 'long']
