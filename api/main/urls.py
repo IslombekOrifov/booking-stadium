6eb5b980f563ba74159f0a132b070559ee4f3894
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-05 05:09:46 +0500

    stadium create url added

--- api/main/urls.py
+++ api/main/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from .views import (
+    StadiumCreateAPIView,
     StadiumListAPIView, StadiumDetailAPIView
 )
 
 
 urlpatterns = [
+    path('create/', StadiumCreateAPIView.as_view()),
     path('list/', StadiumListAPIView.as_view()),
     path('detail/<int:pk>/', StadiumDetailAPIView.as_view()),
+    
 ]
