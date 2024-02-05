c5a3211b74ada0ddf11919a42cc9d124eb75ca7d
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-05 05:10:32 +0500

    stadium create view added

--- api/main/views.py
+++ api/main/views.py
@@ -1,12 +1,41 @@
 from django.http import Http404
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView, RetrieveAPIView,
+    CreateAPIView,
+)
+from rest_framework import status
+from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .serializers import StadiumSerializer, StadiumListSerializer
+from .serializers import (
+    StadiumSerializer, StadiumListSerializer, StadiumImageSerializer,
+    StadiumCreateSerializer
+)
 
 from main.models import Stadium
 
+
+class StadiumCreateAPIView(CreateAPIView):
+    queryset = Stadium.objects.all()
+    serializer_class = StadiumCreateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def create(self, request, *args, **kwargs):
+        images = request.data.pop('images', [])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer, images)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer, images):
+        serializer.save(user=self.request.user)
+        if not images:
+            images_serializer = StadiumImageSerializer(data=images, many=True)
+            images_serializer.is_valid(raise_exception=True)
+            images_serializer.save(stadium=serializer.instance)
+    
 
 class StadiumListAPIView(ListAPIView):
     queryset = Stadium.objects.all()
