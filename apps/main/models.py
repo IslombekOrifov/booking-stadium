bdcde6494772efa75eeba1597220ed661b4ed576
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-05 05:09:26 +0500

    stadium lat long default values removed

--- apps/main/models.py
+++ apps/main/models.py
@@ -17,8 +17,8 @@ class Stadium(models.Model):
     price = models.PositiveIntegerField()
     start_working_time = models.TimeField()
     end_working_time = models.TimeField()
-    lat = models.DecimalField(max_digits=22, decimal_places=18, default=0)
-    long = models.DecimalField(max_digits=22, decimal_places=18, default=0)
+    lat = models.DecimalField(max_digits=22, decimal_places=18)
+    long = models.DecimalField(max_digits=22, decimal_places=18)
     created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
