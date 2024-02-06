d98b4f27ca63b6f4eaf653f7b47e49d5b56a3223
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-07 04:44:45 +0500

    rating urls changed

--- api/main/urls.py
+++ api/main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     StadiumCreateAPIView,
     StadiumListAPIView, StadiumDetailAPIView,
-    RatingAddAPIView
+    RatingCreateOrUpdateAPIView
 )
 
 
@@ -11,5 +11,5 @@ urlpatterns = [
     path('create/', StadiumCreateAPIView.as_view()),
     path('list/', StadiumListAPIView.as_view()),
     path('detail/<int:pk>/', StadiumDetailAPIView.as_view()),
-    path('rate/<int:pk>/', RatingAddAPIView.as_view()),
+    path('rate/<int:pk>/', RatingCreateOrUpdateAPIView.as_view()),
 ]
