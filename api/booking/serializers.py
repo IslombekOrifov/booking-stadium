47d864f53b52eb1f1e1c047c45823743ed80ce65
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-07 04:42:15 +0500

    booking serializers added extra kwargs

--- api/booking/serializers.py
+++ api/booking/serializers.py
@@ -8,3 +8,6 @@ class BookingSerializer(serializers.ModelSerializer):
         model = Booking
         fields = ['stadium', 'user', 'booking_start_time', 
                   'booking_end_time', 'created']
+        extra_kwargs = {
+            'created': {'read_only': True},
+        }
