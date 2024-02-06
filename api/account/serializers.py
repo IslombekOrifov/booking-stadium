e64a3c2e02028a52c53ff46f0c01440878563552
IslombekOrifov <islombekorifov0199@gmail.com>
2024-02-07 04:40:49 +0500

    added register and password change serializers

--- api/account/serializers.py
+++ api/account/serializers.py
@@ -1,6 +1,25 @@
+from django.contrib.auth import password_validation
 from rest_framework import serializers
 
 from account.models import CustomUser
+from main.validators import phone_number_validator
+
+
+class RegisterSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'password', 'phone']
+        
+        
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', '')
+        )
+        return user
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
@@ -8,3 +27,30 @@ class CustomUserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['username', 'first_name', 'last_name', 
                   'phone', 'photo']
+
+
+class UserChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(max_length=128, write_only=True, required=True)
+    new_password1 = serializers.CharField(max_length=128, write_only=True, required=True)
+    new_password2 = serializers.CharField(max_length=128, write_only=True, required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError(
+                'Your old password was entered incorrectly. Please enter it again.'
+            )
+        return value
+
+    def validate(self, data):
+        if data['new_password1'] != data['new_password2']:
+            raise serializers.ValidationError({'new_password2': "The two password fields didn't match."})
+        password_validation.validate_password(data['new_password1'], self.context['request'].user)
+        return data
+
+    def save(self, **kwargs):
+        password = self.validated_data['new_password1']
+        user = self.context['request'].user
+        user.set_password(password)
+        user.save()
+        return user
